dcde6e4495d4f83a117388a9416c438519bfd7c3
AljosaNj <aljosaa.njezic@gmail.com>
2023-01-24 14:15:02 +0100

    envdep

--- client/src/components/Alert.js
+++ client/src/components/Alert.js
@@ -3,7 +3,6 @@ import { useAppContext } from '../context/appContext'
 const Alert = () => {
   const { alertType, alertText } = useAppContext()
   return (
-  
    <div className={`alert alert-${alertType}`}>{alertText}</div>
 
   )
